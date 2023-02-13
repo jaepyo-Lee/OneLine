de9258a4ed5375830bf99d343a2b214d92a231a1
jaepyo99 <jaepyo99@naver.com>
2023-02-14 00:02:08 +0900

    Initial commit

---
+++ src/main/java/com/needle/oneline/OnelineApplication.java
@@ -0,0 +1,13 @@
+package com.needle.oneline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class OnelineApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(OnelineApplication.class, args);
+	}
+}
---
+++ src/test/java/com/needle/oneline/OnelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.needle.oneline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
