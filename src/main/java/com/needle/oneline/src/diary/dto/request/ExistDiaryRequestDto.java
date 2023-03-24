1655d33b8728383be05bafc57ec81277cab9e117
jaepyo99 <jaepyo99@naver.com>
2023-03-24 14:29:56 +0900

    "수정 : 에러 수정"

--- src/main/java/com/needle/oneline/src/diary/dto/request/ExistDiaryRequestDto.java
+++ src/main/java/com/needle/oneline/src/diary/dto/request/ExistDiaryRequestDto.java
@@ -2,13 +2,18 @@ package com.needle.oneline.src.diary.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.PastOrPresent;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+@NoArgsConstructor
 @Getter
 public class ExistDiaryRequestDto {
+
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
     private LocalDate localDate;
 
